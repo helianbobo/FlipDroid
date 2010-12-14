b24019212b990ed3fb10afb54b7edb4b9b071216
unknown <cliu3@.asiapacific.hpqcorp.net>
2010-12-14 13:39:13 +0800

    remove GBK char

--- src/com/goal98/flipdroid/model/FakeArticleSource.java
+++ src/com/goal98/flipdroid/model/FakeArticleSource.java
@@ -5,9 +5,9 @@ import java.util.*;
 public class FakeArticleSource extends AbstractArticleSource {
 
     private final static String strLib = "A hacking free-for-all has exploded on the Web, and Facebook and Twitter are stuck in the middle.\n" +
-            "On Wednesday, anonymous hackers took aim at companies perceived to have harmed WikiLeaks after its release of a flood of confidential diplomatic documents. MasterCard, Visa and PayPal, which had cut off people’s ability to donate money to WikiLeaks, were hit by attacks that tried to block access to the companies’ Web sites and services.\n" +
+            "On Wednesday, anonymous hackers took aim at companies perceived to have harmed WikiLeaks after its release of a flood of confidential diplomatic documents. MasterCard, Visa and PayPal, which had cut off people's ability to donate money to WikiLeaks, were hit by attacks that tried to block access to the companies' Web sites and services.\n" +
             "To organize their efforts, the hackers have turned to sites like Facebook and Twitter. That has drawn these Web giants into the fray and created a precarious situation for them.\n" +
-            "Both Facebook and Twitter — but particularly Twitter — have received praise in recent years as outlets for free speech. Governments trying to control the flow of information have found it difficult to block people from voicing their concerns or setting up meetings through the sites.\n" +
+            "Both Facebook and Twitter but particularly Twitter have received praise in recent years as outlets for free speech. Governments trying to control the flow of information have found it difficult to block people from voicing their concerns or setting up meetings through the sites.\n" +
             "At the same time, both Facebook and Twitter have corporate aspirations that hinge on their ability to serve as ad platforms for other companies. This leaves them with tough public relations and business decisions around how they should handle situations as politically charged as the WikiLeaks developments.";
 
     private static String[] strLibArray;
