c81e680d5291f46f7f7c98c7593e37b9bc57e941
liuchao <liuchao36@gmail.com>
2010-12-22 01:01:09 +0800

    Add menu to index activity

--- app/src/com/goal98/flipdroid/activity/IndexActivity.java
+++ app/src/com/goal98/flipdroid/activity/IndexActivity.java
@@ -5,6 +5,8 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -13,6 +15,8 @@ import com.goal98.flipdroid.R;
 import java.util.ArrayList;
 
 public class IndexActivity extends ListActivity {
+
+    static final private int CONFIG_ID = Menu.FIRST;
 
     private ArrayAdapter<String> mAdapter;
 
@@ -41,5 +45,33 @@ public class IndexActivity extends ListActivity {
         intent.putExtra("repo", (String)l.getItemAtPosition(position));
         startActivity(intent);
         overridePendingTransition(android.R.anim.slide_in_left, R.anim.fade);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        menu.add(0, CONFIG_ID, 0, R.string.config);
+
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case CONFIG_ID:
+                startActivity(new Intent(this, ConfigActivity.class));
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
