4b6a6e75f6383b8c716aacb1e1951a1ef649cb5a
liuchao <liuchao36@gmail.com>
2011-01-05 20:32:46 +0800

    Add SourceDB

--- app/src/com/goal98/flipdroid/db/SourceDB.java
+++ app/src/com/goal98/flipdroid/db/SourceDB.java
@@ -6,33 +6,30 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteQueryBuilder;
-import android.net.Uri;
-import android.text.TextUtils;
 import android.util.Log;
 import com.goal98.flipdroid.util.Constants;
 
-public class AccountDB {
+public class SourceDB {
 
-    private static final String ACCOUNT_TABLE_NAME = "account";
-    private static final String KEY_USERNAME = "username";
-    private static final String KEY_PASSWORD = "password";
+    private static final String TABLE_NAME = "source";
+    private static final String KEY_SOURCE_NAME = "source_name";
     private static final String KEY_ACCOUNT_TYPE = "account_type";
 
     private SQLiteOpenHelper accounthelper;
 
-    public AccountDB(Context context) {
-        accounthelper = new AccountOpenHelper(context);
+    public SourceDB(Context context) {
+        accounthelper = new SourceOpenHelper(context);
     }
 
     public int deleteAll() {
 
         SQLiteDatabase db = accounthelper.getWritableDatabase();
-        return db.delete(ACCOUNT_TABLE_NAME, null, null);
+        return db.delete(TABLE_NAME, null, null);
     }
 
     public Cursor query(String[] projection, String selection, String[] selectionArgs, String sortOrder) {
         SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
-        qb.setTables(ACCOUNT_TABLE_NAME);
+        qb.setTables(TABLE_NAME);
 
         SQLiteDatabase db = accounthelper.getReadableDatabase();
         Cursor cursor = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder);
@@ -42,37 +39,36 @@ public class AccountDB {
 
     public long insert(ContentValues values) {
         SQLiteDatabase db = accounthelper.getWritableDatabase();
-        return db.insert(ACCOUNT_TABLE_NAME, KEY_ACCOUNT_TYPE, values);
+        return db.insert(TABLE_NAME, KEY_ACCOUNT_TYPE, values);
     }
 
-    public long insert(String username, String password, String accountType) {
+    public long insert(String sourceName, String accountType) {
         ContentValues values = new ContentValues();
-        values.put(KEY_USERNAME, username);
-        values.put(KEY_PASSWORD, password);
+        values.put(KEY_SOURCE_NAME, sourceName);
         values.put(KEY_ACCOUNT_TYPE, accountType);
         return insert(values);
     }
 
     public int update(ContentValues values, String where, String[] whereArgs) {
         SQLiteDatabase db = accounthelper.getWritableDatabase();
-        int count = db.update(ACCOUNT_TABLE_NAME, values, where, whereArgs);
+        int count = db.update(TABLE_NAME, values, where, whereArgs);
         return count;
     }
 
-    public static class AccountOpenHelper extends SQLiteOpenHelper {
 
-        private static final int DATABASE_VERSION = 2;
+    public static class SourceOpenHelper extends SQLiteOpenHelper {
+
+        private static final int DATABASE_VERSION = 1;
 
         private static final String ACCOUNT_TABLE_CREATE =
-                "CREATE TABLE " + ACCOUNT_TABLE_NAME + " (" +
+                "CREATE TABLE " + TABLE_NAME + " (" +
                         KEY_ACCOUNT_TYPE + " TEXT, " +
-                        KEY_USERNAME + " TEXT, " +
-                        KEY_PASSWORD + " TEXT, " +
-                        "PRIMARY KEY ("+KEY_ACCOUNT_TYPE + "," + KEY_USERNAME +")" +
+                        KEY_SOURCE_NAME + " TEXT, " +
+                        "PRIMARY KEY ("+KEY_ACCOUNT_TYPE + "," + KEY_SOURCE_NAME +")" +
                         ");";
 
 
-        public AccountOpenHelper(Context context) {
+        public SourceOpenHelper(Context context) {
             super(context, Constants.DATABASE_NAME, null, DATABASE_VERSION);
         }
 
@@ -86,7 +82,7 @@ public class AccountDB {
 
             Log.w(this.getClass().getName(), "Upgrading database from version " + oldVersion + " to "
                     + newVersion + ", which will destroy all old data");
-            db.execSQL("DROP TABLE IF EXISTS " + ACCOUNT_TABLE_NAME);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
             onCreate(db);
 
         }
