4141a8967d19bd654befa1809f9a2741a9286c17
liuchao <liuchao36@gmail.com>
2011-01-09 17:36:13 +0800

    Add Sina Account Activity

--- app/gen/com/goal98/flipdroid/R.java
+++ app/gen/com/goal98/flipdroid/R.java
@@ -28,10 +28,10 @@ public final class R {
         public static final int twitter=0x7f020005;
     }
     public static final class id {
-        public static final int button=0x7f090005;
         public static final int currentPage=0x7f090001;
         public static final int nextPage=0x7f090002;
         public static final int pageContainer=0x7f090000;
+        public static final int sina_login=0x7f090005;
         public static final int sina_password=0x7f090004;
         public static final int sina_username=0x7f090003;
         public static final int siteGrid=0x7f090006;
@@ -47,18 +47,21 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f070000;
-        public static final int button_add_new_account=0x7f07000b;
-        public static final int button_login=0x7f07000c;
-        public static final int clear_all_account=0x7f070002;
+        public static final int button_add_new_account=0x7f07000e;
+        public static final int button_login=0x7f07000f;
+        public static final int clear_all_account=0x7f070004;
         public static final int config=0x7f070001;
-        public static final int dialog_title_anim_flip_duration_preference=0x7f070006;
-        public static final int dialog_title_article_per_page_preference=0x7f07000a;
-        public static final int key_anim_flip_duration_preference=0x7f070003;
-        public static final int key_article_per_page_preference=0x7f070007;
-        public static final int summary_anim_flip_duration_preference=0x7f070005;
-        public static final int summary_article_per_page_preference=0x7f070009;
-        public static final int title_anim_flip_duration_preference=0x7f070004;
-        public static final int title_article_per_page_preference=0x7f070008;
+        public static final int dialog_title_anim_flip_duration_preference=0x7f070009;
+        public static final int dialog_title_article_per_page_preference=0x7f07000d;
+        public static final int key_anim_flip_duration_preference=0x7f070006;
+        public static final int key_article_per_page_preference=0x7f07000a;
+        public static final int password=0x7f070002;
+        public static final int sina_weibo=0x7f070005;
+        public static final int summary_anim_flip_duration_preference=0x7f070008;
+        public static final int summary_article_per_page_preference=0x7f07000c;
+        public static final int title_anim_flip_duration_preference=0x7f070007;
+        public static final int title_article_per_page_preference=0x7f07000b;
+        public static final int username=0x7f070003;
     }
     public static final class style {
         public static final int Article=0x7f08000a;
--- app/src/com/goal98/flipdroid/activity/SinaAccountActivity.java
+++ app/src/com/goal98/flipdroid/activity/SinaAccountActivity.java
@@ -1,7 +1,11 @@
 package com.goal98.flipdroid.activity;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import com.goal98.flipdroid.R;
 
 
@@ -10,5 +14,12 @@ public class SinaAccountActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.sina_account);
+
+        Button button = (Button)findViewById(R.id.sina_login);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Log.e(this.getClass().getName(), "Clicked!");
+            }
+        });
     }
 }
