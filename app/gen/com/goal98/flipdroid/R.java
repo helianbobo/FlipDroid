3ca5280370dc414f16e46158fba3b1c6c4c02ff5
liuchao <liuchao36@gmail.com>
2011-01-10 21:08:14 +0800

    Add Logo

--- app/gen/com/goal98/flipdroid/R.java
+++ app/gen/com/goal98/flipdroid/R.java
@@ -21,11 +21,12 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
-        public static final int portrait=0x7f020001;
-        public static final int portrait_small=0x7f020002;
-        public static final int sina=0x7f020003;
-        public static final int tencent=0x7f020004;
-        public static final int twitter=0x7f020005;
+        public static final int logo=0x7f020001;
+        public static final int portrait=0x7f020002;
+        public static final int portrait_small=0x7f020003;
+        public static final int sina=0x7f020004;
+        public static final int tencent=0x7f020005;
+        public static final int twitter=0x7f020006;
     }
     public static final class id {
         public static final int currentPage=0x7f090001;
@@ -47,20 +48,21 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f070000;
-        public static final int button_add_new_account=0x7f07000e;
-        public static final int button_login=0x7f07000f;
+        public static final int button_add_new_account=0x7f07000f;
+        public static final int button_login=0x7f070010;
         public static final int clear_all_account=0x7f070004;
         public static final int config=0x7f070001;
-        public static final int dialog_title_anim_flip_duration_preference=0x7f070009;
-        public static final int dialog_title_article_per_page_preference=0x7f07000d;
-        public static final int key_anim_flip_duration_preference=0x7f070006;
-        public static final int key_article_per_page_preference=0x7f07000a;
+        public static final int dialog_title_anim_flip_duration_preference=0x7f07000a;
+        public static final int dialog_title_article_per_page_preference=0x7f07000e;
+        public static final int flip=0x7f070005;
+        public static final int key_anim_flip_duration_preference=0x7f070007;
+        public static final int key_article_per_page_preference=0x7f07000b;
         public static final int password=0x7f070002;
-        public static final int sina_weibo=0x7f070005;
-        public static final int summary_anim_flip_duration_preference=0x7f070008;
-        public static final int summary_article_per_page_preference=0x7f07000c;
-        public static final int title_anim_flip_duration_preference=0x7f070007;
-        public static final int title_article_per_page_preference=0x7f07000b;
+        public static final int sina_weibo=0x7f070006;
+        public static final int summary_anim_flip_duration_preference=0x7f070009;
+        public static final int summary_article_per_page_preference=0x7f07000d;
+        public static final int title_anim_flip_duration_preference=0x7f070008;
+        public static final int title_article_per_page_preference=0x7f07000c;
         public static final int username=0x7f070003;
     }
     public static final class style {
