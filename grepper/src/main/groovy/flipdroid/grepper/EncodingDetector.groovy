2f674cd291dfdac2a1d1c3d0ffc72a6fd5fa8782
JLeo <jleo@jleo-VirtualBox.(none)>
2011-03-16 23:10:15 +0800

    add sniff meta info strategy of detecting encoding

--- grepper/src/main/groovy/flipdroid/grepper/EncodingDetector.groovy
+++ grepper/src/main/groovy/flipdroid/grepper/EncodingDetector.groovy
@@ -4,6 +4,8 @@ import org.mozilla.intl.chardet.nsDetector
 import org.mozilla.intl.chardet.nsPSMDetector
 import org.mozilla.intl.chardet.nsICharsetDetectionObserver
 import org.mozilla.intl.chardet.HtmlCharsetDetector
+import java.util.regex.Pattern
+import java.util.regex.Matcher
 /**
  * Created by IntelliJ IDEA.
  * User: Administrator
@@ -12,13 +14,40 @@ import org.mozilla.intl.chardet.HtmlCharsetDetector
  * To change this template use File | Settings | File Templates.
  */
 class EncodingDetector {
+  private static final int CHUNK_SIZE = 2000;
+  private static Pattern metaPattern =
+  Pattern.compile("<meta\\s+([^>]*http-equiv=\"?content-type\"?[^>]*)>",
+          Pattern.CASE_INSENSITIVE);
+  private static Pattern charsetPattern =
+  Pattern.compile("charset=\\s*([a-z][_\\-0-9a-z]*)",
+          Pattern.CASE_INSENSITIVE);
+
+
+  private static String sniffCharacterEncoding(byte[] content) {
+    int length = content.length < CHUNK_SIZE ?
+      content.length : CHUNK_SIZE;
+
+    String str = new String(content, 0, 0, length);
+
+    Matcher metaMatcher = metaPattern.matcher(str);
+    String encoding = null;
+    if (metaMatcher.find()) {
+      Matcher charsetMatcher = charsetPattern.matcher(metaMatcher.group(1));
+      if (charsetMatcher.find())
+        encoding = new String(charsetMatcher.group(1));
+    }
+
+    return encoding;
+  }
+
+
   public static String detect(urlStr) {
     URL url = new URL(urlStr);
     BufferedInputStream imp = new BufferedInputStream(url.openStream());
     return detect(imp)
   }
 
-  public static String detect(InputStream imp){
+  public static String detect(InputStream imp) {
     def det = new nsDetector(nsPSMDetector.ALL);
     String detectedCharset
     det.Init([
@@ -32,7 +61,21 @@ class EncodingDetector {
     boolean done = false;
     boolean isAscii = true;
 
+    byte[] first2000Byte = new byte[2048];
+    int temp = 0;
+    boolean sniffFail = false;
     while ((len = imp.read(buf, 0, buf.length)) != -1) {
+      if (!sniffFail) {
+        System.arraycopy(buf, 0, first2000Byte, temp, len);
+        temp += len;
+        if (temp > 2000) {
+          String encoding = sniffCharacterEncoding(first2000Byte);
+          if (encoding != null)
+            return encoding;
+          else
+            sniffFail = true;
+        }
+      }
       if (isAscii)
         isAscii = det.isAscii(buf, len);
 
@@ -45,6 +88,6 @@ class EncodingDetector {
     if (isAscii) {
       detectedCharset = "ASCII"
     }
-   return detectedCharset
+    return detectedCharset
   }
 }
