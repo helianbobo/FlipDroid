a809880fc428911d25fcda406cc65ef88bd8bd04
JLeo <ggyyleo@gmail.com>
2011-03-16 03:13:37 -0700

    add more terminating words

--- boilerpipe/src/main/java/de/l3s/boilerpipe/filters/english/TerminatingBlocksFinder.java
+++ boilerpipe/src/main/java/de/l3s/boilerpipe/filters/english/TerminatingBlocksFinder.java
@@ -89,6 +89,9 @@ public class TerminatingBlocksFinder implements BoilerpipeFilter {
                         || text.contains("扩展阅读")
                         || text.contains("赞")
                         || text.contains("更多")
+                        || text.toUpperCase().contains("RELATED TOPICS")
+                        || text.toUpperCase().contains("SHARE IT ON")
+                        || text.toUpperCase().contains("SHARE THIS ON")
                 ) {
                     tb.addLabel(DefaultLabels.INDICATES_END_OF_TEXT);
                     changes = true;
