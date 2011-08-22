e824466b80e9d2c4d1603b87de87e77c6c9b4bb4
rzshang@gmail.com <rzshang@gmail.com>
2011-08-22 19:37:17 +0800

    Signed-off-by: rzshang@gmail.com <rzshang@gmail.com>

--- tika-schedule/callwebui/views.py
+++ tika-schedule/callwebui/views.py
@@ -6,6 +6,9 @@ import model
 import time
 ISOTIMEFORMAT='%Y-%m-%d %X'
 
+def test(request):
+    return HttpResponse('showtest')
+
 def paging(request,item_list,per_page=10):
     paginator = Paginator(item_list,per_page)
     p = request.GET.get('page','')
@@ -26,28 +29,28 @@ def showSource(request):
     return render_to_response('source.html', {'items': items})
 
 def updateSource(request):
-    if request.POST['update']=='add':
+    if request.GET['update']=='add':
         x = model.con.Source() 
-        x["url"]=request.POST['url']
-        x["type"]=request.POST['type']
+        x["url"]=request.GET['url']
+        x["type"]=request.GET['type']
         x["md5"] = u"1"
         x["time"]=unicode(time.strftime(ISOTIMEFORMAT, time.gmtime()))
          
         x.save()
         return HttpResponseRedirect("/source/")
         
-    type = request.POST['type'] 
-    url = request.POST['url']
-    theid = request.POST['_id']
+    type = request.GET['type'] 
+    url = request.GET['url']
+    theid = request.GET['_id']
   
-    if request.POST['update']=='update':
+    if request.GET['update']=='update':
         model.con.tika.source.update({'_id':pymongo.objectid.ObjectId(theid)}, 
                                   {'$set':{"url": url,
                                             "time": time.strftime( ISOTIMEFORMAT, time.gmtime())                              
                                 }},False) 
         return HttpResponseRedirect("/source/")
 
-    if request.POST['update']=='delete':
+    if request.GET['update']=='delete':
         model.con.tika.source.remove({'_id':pymongo.objectid.ObjectId(theid)})
         return HttpResponseRedirect("/source/")
     
@@ -62,14 +65,14 @@ def showUrls(request):
 
 def updateUrls(request):        
    
-    theid = request.POST['_id']
+    theid = request.GET['_id']
   
-    if request.POST['update']=='show':
+    if request.GET['update']=='show':
         item = model.con.tika.url_abstract.find({'_id':pymongo.objectid.ObjectId(theid)})
         html = item[0]['content']
         return HttpResponse(html)
 
-    if request.POST['update']=='delete':
+    if request.GET['update']=='delete':
         model.con.tika.url_abstract.remove({'_id':pymongo.objectid.ObjectId(theid)})
         return HttpResponseRedirect("/urls/")
         
